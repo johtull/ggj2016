76c327217ecf3dd9cd46e0fbbfbafa88215a110f
MangoDood <isonok@net.elmhurst.edu>
2016-01-30 16:42:13 -0600

    Transition Screens
    Ok this good enough for placeholder stuff?

---
+++ Assets/Scripts/AutoTransition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoTransition : MonoBehaviour {
+    int times = 300;
+	// Use this for initialization
+	void Start () {
+        times = 300;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        times = times - 1;
+        if (times <= 0)
+        Application.LoadLevel("TitleScreen");
+    }
+}
---
+++ Assets/Scripts/StartButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void ChangeSceneStart (string sceneChange)
+    {
+        Application.LoadLevel(sceneChange);
+    }
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
