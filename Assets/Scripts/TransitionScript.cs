c2f9f80bb88c04e9aef7889f2ac22bfa472f76ac
MangoDood <isonok@net.elmhurst.edu>
2016-01-31 11:09:49 -0600

    Transition Screen Fixes
    Here you go

---
+++ Assets/Scripts/TransitionScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransitionScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.LoadLevel("MainTestRoom");
+        }
+
+    }
+}
